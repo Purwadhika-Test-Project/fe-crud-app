d6d3b4b5aafc0b70cd742b76141ea1e8fa6cc5aa
bagasdhitya98 <bagasdhityataufiqqi21@gmail.com>
2024-03-24 09:44:54 +0700

    add: added input and button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import { ButtonProps } from "@/utils/interface";
+
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
+  return (
+    <button
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
